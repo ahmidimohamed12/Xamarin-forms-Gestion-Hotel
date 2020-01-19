52573235445a3ea6a7bdd5a18ad48a50dd79de9e
Ahmidi Mohamed <ahmidinador123@gmail.com>
2020-01-19 02:26:05 +0100

    .

--- App8/App8/App8/obj/Debug/netstandard2.0/Page1.xaml.g.cs
+++ App8/App8/App8/obj/Debug/netstandard2.0/Page1.xaml.g.cs
@@ -17,8 +17,12 @@ namespace App8 {
     public partial class Page1 : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Maps.Map maps;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Page1));
+            maps = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Maps.Map>(this, "maps");
         }
     }
 }
